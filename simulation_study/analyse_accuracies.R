5391514baf519ba068c1e3dbaa66c9aceeb80221
mfhstdt <madlenhoffstadt@t-online.de>
2024-07-08 20:11:35 +0200

    Added simulation study

---
+++ simulation_study/analyse_accuracies.R
@@ -0,0 +1,176 @@
+#       SIMULATION STUDY: COMPUTE ACCURACY PER MODALITY DETECTION METHOD        # 
+#################################################################################
+
+# What happens here? ------------------------------------------------------------#
+# Script 2: 
+# Compare detected modality with true modality to compute accuracy per method
+# This is done for A) discriminating uni- from multimodality for all methods 
+# and B) in detecting the number of modes, where applicable 
+# C) Plots results (replicating Fig. 3 and Fig. 4 from ReMoDe paper)
+#--------------------------------------------------------------------------------#
+
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+
+# load all results from script 2
+rdata_files <- list.files(path = "simulation_study/results", pattern = "\\.RData$", full.names = TRUE)
+for (file in rdata_files) {load(file)}
+
+# merge all results 
+results_df <- rbind(silverman, dip, ACR, GMM, BC, DensMM, dfu, remode)
+
+# A) accuracy in discriminating uni- from multimodality -------------------------#
+acc_multimod <- results_df %>%
+  mutate(correct = ifelse((true_modes > 1 & multimod == TRUE | true_modes == 1 & multimod == FALSE), 1, 0)) %>%
+  group_by(method,n) %>%
+  summarise(acc = mean(correct, na.rm = TRUE))
+acc_multimod
+
+# B) accuracy in detecting number of modes --------------------------------------#
+acc_modality <- results_df %>%
+  filter(method != "dip" & method != "BC" & method != "DFU" & method != "ACR multimodality") %>%
+  mutate(corr = ifelse(true_modes == modality_est, 1, 0)) %>%
+  group_by(method) %>%
+  summarise(acc = mean(corr, na.rm=TRUE))
+acc_modality
+
+
+# C) plot results ---------------------------------------------------------------------------------#
+
+# code distribution type
+results_df$dist_type <- c()
+for(i in 1:nrow(results_df)){
+  results_df$dist_type[i] <- strsplit(results_df$name[i], "_N")[[1]][1]
+}
+results_df$dist_type2 <- ifelse(grepl("Trimodal_Gaussians", results_df$dist_type), "Trimodal Gaussians",
+                             ifelse(grepl("Bimodal_Gaussians", results_df$dist_type), "Bimodal Gaussians",
+                                    ifelse(grepl("Bimodal_Special_case", results_df$dist_type), "Bimodal Special Case",
+                                           ifelse(grepl("Unimodal_Beta", results_df$dist_type), "Unimodal Beta", 
+                                                  results_df$dist_type))))
+results_df$dist_type2 <- gsub("_", " ", results_df$dist_type2)
+results_df$dist_type3 <- ifelse((results_df$dist_type2 == "Unimodal Beta" | results_df$dist_type2 == "Unimodal Special case 13"), 
+                             "Unimodal Non-Gaussian", 
+                             ifelse((grepl("Bimodal", results_df$dist_type2) & results_df$dist_type2 != "Bimodal Gaussians"), 
+                                    "Bimodal Non-Gaussian", 
+                                    ifelse(results_df$dist_type2 == "Trimodal beta mixtures", "Trimodal Non-Gaussian",
+                                           results_df$dist_type2)))
+results_df$dist_type3 <- gsub("_", " ", results_df$dist_type3)
+
+
+# FIGURE 3 - discriminating uni- from multimodality ----------#
+results_df$correct_multimod <- ifelse((results_df$true_modes > 1 & results_df$multimod == TRUE) 
+                                      |  (results_df$true_modes == 1 & results_df$multimod == FALSE),
+                                   1, 0)
+
+multimod_accuracies4 <- results_df %>% 
+  group_by(dist_type3, n, method) %>%
+  summarize(acc = round(mean(correct_multimod),3)) %>%
+  tidyr::pivot_wider(
+    id_cols = c(dist_type3, n), 
+    names_from = method,   
+    values_from = acc  
+  )
+
+multimod4_long <- multimod_accuracies4 %>%
+  tidyr::pivot_longer(
+    cols = !c(dist_type3, n), 
+    names_to = "method",
+    values_to = "mean_accuracy"
+  )
+
+colors <- c("DensMM" = "#79C3B8", "Remode" = "#ED6A5A" , "silverman" = "#5CA4A9",
+            "GMM" = "black", "ACR multimodality" = "black",
+            "BC" = "black", "dip" = "black", "DFU" = "black")
+
+gg_list <- list()
+
+for (i in c( "DFU","ACR multimodality", "Remode",  "silverman", "DensMM", "GMM", "ACR multimodality" ,"BC", "dip", "DFU")) {
+  subset <- multimod4_long[multimod4_long$method == i,]
+  gg <- ggplot(subset, aes(x = interaction(factor(n), dist_type3), y = mean_accuracy, 
+                           group = dist_type3, color = method)) +
+    geom_hline(yintercept = 1, linetype = "dashed", color = "darkgrey") +
+    geom_line(position = position_dodge(width = 0.5),  show.legend = FALSE) +
+    geom_point(position = position_dodge(width = 0.5),  show.legend = FALSE) +
+    labs(x = " ", y = "Accuracy", title = i) +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    scale_color_manual(values = colors)
+  
+  # Include legend only in the first plot
+  # if (i == "BC") {
+  #   gg <- gg + scale_color_discrete(name = "Distribution Type")
+  # } else {
+  #   gg <- gg + theme(legend.position = "none")
+  # }
+  # only x axis labels for last plot 
+  if (i == "dip") {
+    gg <- gg + scale_x_discrete(labels = rep(unique(subset$n), 8))
+  } else {
+    gg <- gg + scale_x_discrete(labels = NULL)
+  }
+  
+  gg_list[[i]] <- gg
+  
+}
+
+# Arrange plots in a grid
+gg_plot_grid <- wrap_plots(gg_list, ncol = 1)
+
+# Display the grid of plots
+gg_plot_grid
+
+
+
+# FIGURE 4 - Detecting number of modes ------------------------#
+
+# data frame with methods that detect the number of modes
+df <- results_df[results_df$method %in% c("silverman", "DensMM", "Remode"), ]
+
+df$correct_modality <- ifelse(df$true_modes == df$modality_est, 1, 0)
+
+df_correct <- df %>% 
+  group_by(dist_type3, n, method) %>%
+  summarize(acc = round(mean(correct_modality, na.rm = TRUE),3)) %>%
+  tidyr::pivot_wider(
+    id_cols = c(dist_type3, n), 
+    names_from = method,   
+    values_from = acc  
+  )
+
+df_long <- df_correct %>%
+  tidyr::pivot_longer(
+    cols = !c(dist_type3, n), 
+    names_to = "method",
+    values_to = "mean_accuracy"
+  )
+
+
+# plot (Fig. 4 in paper)
+method_linetypes <- c("DensMM" = "dashed", "Remode" = "solid", "silverman" = "dotted")
+method_colors <- c("DensMM" = "#79C3B8", "Remode" = "#ED6A5A" , "silverman" = "#5CA4A9")
+ggplot(df_long, aes(x = factor(n), y = mean_accuracy, color = method, 
+                    linetype = method, shape = method, group = method)) +
+  geom_line(size = 1, alpha = 0.5) +
+  geom_point(size = 3) +
+  facet_wrap(~ dist_type3, scales = "free_x", nrow = 4, ncol = 2) +
+  scale_linetype_manual(values = method_linetypes) +  
+  scale_color_manual(values = method_colors) +       
+  scale_shape_manual(values = c(15, 16, 17, 18)) +   
+  labs(
+    title = "Mean Accuracy in Detecting the Correct Number of Modes",
+    subtitle = "by Sample Size and Method",
+    x = "Sample Size (n)",
+    y = "Mean Accuracy",
+    color = "Method",
+    linetype = "Method",
+    shape = "Method"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "top",
+    strip.text = element_text(size = 12), 
+    panel.spacing = unit(1, "lines")  
+  )
---
+++ simulation_study/apply_methods.R
@@ -0,0 +1,271 @@
+# SIMULATION STUDY: APPLY MODALITY DETECTION METHODS TO BENCHMARK DISTRIBUTIONS # 
+#################################################################################
+
+# What happens here? ------------------------------------------------------------#
+# Script 1: 
+# Established and new methods are applied to detect the number of modes in 
+# 172 simulated distributions of ordinal data. 
+# NOTE: some simulations are highly computationally expensive 
+#--------------------------------------------------------------------------------#
+
+
+# LOAD ALL FUNCTIONS AND SIMULATED DISTRIBUTIONS ---------------------------------#
+
+library(parSim)
+library(dplyr)
+library(stringr)
+
+# load remode package
+# A) either run devtools::install_github("hvdmaas/remode")
+# B) or download R project here: https://github.com/hvdmaas/remode
+#    open R project and run devtools::load_all()
+
+# load functions for established modality detection methods 
+source("simulation_study/source_alternative_functions.R")
+
+# load benchmark distribution 
+load("simulation_study/benchmark_distributions.RData")
+
+
+# APPLY FUNCTIONS TO BENCHMARK DISTRIBUTIONS -------------------------------------#
+
+# keep track of distributions & true modality
+true_modes <- ifelse(grepl("Uni", names(benchmark_distributions)), 1, 
+                     ifelse(grepl("Bimodal", names(benchmark_distributions)), 2, 
+                            ifelse(grepl("Trimodal", names(benchmark_distributions)), 3, 5)))
+overview_benchmark <- data.frame(  # store info on distributions
+  name = names(benchmark_distributions),
+  i = 1:length(true_modes), 
+  n = c(rep(rep(c(50,500,5000,50000), each = 2), 21), 50,500,5000,50000), 
+  c = c(rep(rep(c(5, 10), 4), 21), c(39, 28, 38, 42)),
+  true_modes = true_modes
+)
+
+
+# Apply silvermans method --------------------------------------#
+test_silverman <- function(i, benchmark){
+  silverman_it(as.numeric(benchmark[[i]]))
+}
+
+parSim(
+  i = 67:68, # index of benchmark distribution
+  reps = 1, 
+  write = T, 
+  export = c("silverman.test","silverman_it",
+             "benchmark_distributions","test_silverman"),
+  name = "benchmark_silverman", 
+  nCores = 8, 
+  expression = {
+    test_silverman(i=i, benchmark=benchmark_distributions)
+  }
+)
+
+silverman_df <- read.table("silverman_results.txt", 
+                           header = TRUE)
+silverman_df <- left_join(silverman_df, overview_benchmark, by="i") %>%
+  select(i, name, n, c, true_modes, df) %>%
+  rename(modality_est = df) %>%
+  mutate(multimod = modality_est > 1, 
+         method = "silverman")
+
+save(silverman_df, file = "simulation_study/results/results_silverman.RData")
+
+
+# Apply dip statistic -----------------------------------------# 
+test_dip <- function(i, benchmark){
+  x <- as.numeric(benchmark[[i]])
+  res <- dip.test(x)
+  res$p.value < 0.01
+}
+
+parSim(
+  i = 1:172, # index of benchmark distribution
+  reps = 1,
+  write = T, 
+  export = c("dip.test","benchmark_distributions","test_dip"),
+  name = "benchmark_dip", 
+  nCores = 8,
+  expression = {
+    test_dip(i=i, benchmark=benchmark_distributions)
+  }
+)
+
+dip_df <- read.table("benchmark_dip.txt", header = TRUE)
+dip_df <- left_join(dip_df, overview_benchmark, by = "i") %>%
+  select(i, name, n, c, true_modes, df) %>%
+  rename(multimod = df) %>%
+  mutate(modality_est = NA, 
+         method = "dip")
+
+save(dip_df, file = "simulation_study/results/results_dip.RData")
+
+
+# Ameijeriras-Alonso method for uni- vs. multimodality ------------------------#
+test_ACR <- function(i, benchmark){
+  x <- as.numeric(benchmark[[i]])
+  res <- modetest(as.numeric(x), B = 50, mod0 = 1)
+  res$p.value <= 0.01 # return if multimodal
+}
+
+parSim(
+  i = 67:80, # index of benchmark distribution
+  reps = 1,
+  write = T, 
+  export = c("test_ACR","benchmark_distributions","modetest"),
+  name = "benchmark_ACR_unimodality", 
+  nCores = 8,
+  expression = {
+    test_ACR(i=i, benchmark=benchmark_distributions)
+  }
+)
+ACR_df <- read.table("benchmark_ACR_unimodality.txt", header = TRUE)
+ACR_df <- left_join(ACR_df, overview_benchmark, by="i") %>%
+  rename(multimod = df) %>%
+  mutate(modality_est = NA, 
+         method = "ACR multimodality") %>%
+  select(i, name, n, c, true_modes, multimod, modality_est, method)
+
+save(ACR_df, file = "simulation_study/results/results_ACR_multimod.RData")
+
+
+# apply Gaussian Mixture Modeling ---------------------#
+test_GMM <- function(i, benchmark){
+  x <- as.numeric(benchmark[[i]])
+  res <- summary(mclustBIC(x))[3]
+  as.numeric(str_split(names(res), ",")[[1]][2])
+}
+
+parSim(
+  i = 1:172, # index of benchmark distribution
+  reps = 1, 
+  write = T, 
+  export = c("mclustBIC","benchmark_distributions","test_GMM","str_split"),
+  name = "benchmark_GMM", 
+  nCores = 8,
+  expression = {
+    test_GMM(i=i, benchmark=benchmark_distributions)
+  }
+)
+
+GMM_df <- read.table("benchmark_GMM.txt", header = TRUE)
+GMM_df <- left_join(GMM_df, overview_benchmark, by="i") %>%
+  mutate(multimod = df > 1,
+         method = "GMM") %>%
+  rename(modality_est = df) %>%
+  select(i, name, n, c, true_modes, multimod, modality_est, method)
+
+save(GMM_df, file = "simulation_study/results/results_GMM.RData")
+
+# Apply Bimodality coefficient --------------------------# 
+test_BC <- function(i, benchmark){
+  bimodality_coefficient(as.numeric(benchmark[[i]]))
+}
+
+parSim(
+  i = 1:172, # index of benchmark distribution
+  reps = 1, 
+  write = T, 
+  export = c("bimodality_coefficient","benchmark_distributions","test_BC"),
+  name = "benchmark_BC", 
+  nCores = 8,
+  expression = {
+    test_BC(i=i, benchmark=benchmark_distributions)
+  }
+)
+
+BC_df <- read.table("benchmark_BC.txt", header = TRUE) 
+BC_df <- left_join(BC_df, overview_benchmark, by="i") %>%
+  select(i, name, n, c, true_modes, df) %>%
+  mutate(multimod = df > (5/9),
+         modality_est = NA, 
+         method = "BC") %>%
+  select(i, name, n, c, true_modes, multimod, modality_est, method)
+
+save(BC_df, file = "simulation_study/results/results_BC.RData")
+
+
+# Apply DensMM method (Haslbeck et al)--------------------------#
+test_densMM <- function(i, benchmark){ # treating claw as contiuous
+  if (i < 169)  DensMMdet(as.numeric(benchmark[[i]]))$M else DensMMdet(as.numeric(benchmark[[i]]), categorical = FALSE)$M
+}
+
+parSim(
+  i = 1:172, # index of benchmark distribution
+  reps = 1, 
+  write = T, 
+  export = c("DensMMdet","benchmark_distributions","test_densMM"),
+  name = "benchmark_densMM", 
+  nCores = 8,
+  expression = {
+    test_densMM(i=i, benchmark=benchmark_distributions)
+  }
+)
+
+DensMM_df <- read.table("benchmark_densMM.txt", header = TRUE)
+DensMM_df <- left_join(DensMM_df, overview_benchmark, by="i") %>%
+  select(i, name, n, c, true_modes, df) %>%
+  mutate(multimod = df > 1, 
+         method = "DensMM") %>%
+  rename(modality_est = df) %>%
+  select(i, name, n, c, true_modes, multimod, modality_est, method)
+
+
+save(DensMM_df, file = "simulation_study/results/results_DensMM.RData")
+
+
+# Apply DFU -------------------------------------------------#
+test_dfu <- function(i, benchmark){
+  dfu(as.numeric(benchmark[[i]]))
+}
+
+parSim(
+  i = 1:172, # index of benchmark distribution
+  reps = 1, 
+  write = T, 
+  export = c("dfu","benchmark_distributions","test_dfu"),
+  name = "benchmark_dfu", 
+  nCores = 8,
+  expression = {
+    test_dfu(i=i, benchmark=benchmark_distributions)
+  }
+)
+
+DFU_df <- read.table("benchmark_dfu.txt", header = TRUE)
+DFU_df <- left_join(DFU_df, overview_benchmark, by="i") %>%
+  select(i, name, n, c, true_modes, df) %>%
+  mutate(multimod = df > 0,
+         modality_est = NA,
+         method = "DFU") %>%
+  select(i, name, n, c, true_modes, multimod, modality_est, method)
+
+save(DFU_df, file = "alternative_methods/results_DFU.RData")
+
+
+# Apply Remode-----------------------------------------------------------------#
+test_Remode <- function(i, benchmark){
+  remode(table(benchmark[i]), alpha_correction = "max_modes")$nr_of_modes[1] 
+}
+
+parSim(
+  i = c(1:172), # index of benchmark distribution
+  reps = 1, 
+  write = T, 
+  export = c("test_Remode","benchmark_distributions","remode","remode_find_maxima", "perform_binomial_test"),
+  name = "benchmark_Remode", 
+  nCores = 8,
+  expression = {
+    test_Remode(i=i, benchmark=benchmark_distributions)
+  }
+)
+Remode_df <- read.table("benchmark_Remode.txt", header = TRUE)
+Remode_df <- left_join(Remode_df, overview_benchmark, by="i") %>%
+  select(i, name, n, c, true_modes, df) %>%
+  rename(modality_est = df) %>%
+  mutate(multimod = modality_est > 1,
+         method = "Remode") %>%
+  select(i, name, n, c, true_modes, multimod, modality_est, method)
+
+save(Remode_df, file = "simulation_study/results/results_Remode.RData")
+
+
+# For accuracy analysis of results, see Script 2: simulation_study/analyse_accuracies.R
